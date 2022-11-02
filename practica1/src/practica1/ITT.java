42edd895ab97f95591668e2c22df7d6c7fc6cf24
José Miguel Márquez Herreros <jmiguelmh3000@gmail.com>
2022-11-02 18:43:43 +0100

    Completado hasta Hon2

--- practica1/src/practica1/ITT.java
+++ practica1/src/practica1/ITT.java
@@ -9,6 +9,7 @@ import ai.Plan;
 import geometry.Point3D;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
+import java.util.ArrayList;
 import tools.emojis;
 import world.Perceptor;
 
@@ -36,7 +37,10 @@ public class ITT extends LARVAFirstAgent {
             problem = "",
             problemManager = "", 
             sessionManager, 
-            content, 
+            content,
+            dest = "",
+            report = "",
+            actualCity,
             sessionKey;
     ACLMessage open, session;
     String[] contentTokens;
@@ -49,10 +53,11 @@ public class ITT extends LARVAFirstAgent {
     Plan behaviour = null;
     Environment Ei, Ef;
     Choice a;
-    Boolean goalInitiated = false;
+    Boolean goalInitiated = false, destFounded = false;
 
     @Override
     public void setup() {
+        this.enableDeepLARVAMonitoring();
         super.setup();
         logger.onTabular();
         myStatus = Status.START;
@@ -219,7 +224,7 @@ public class ITT extends LARVAFirstAgent {
     public Status MySolveProblem() {
         String[] aux = getEnvironment().getCurrentGoal().split(" ");
         Type type = Type.valueOf(aux[0]);
-        Status status;
+        Status status = null;
         
         switch(type) {
             case MOVEIN:
@@ -239,22 +244,45 @@ public class ITT extends LARVAFirstAgent {
             case LIST:
                 status = doQueryPeople(aux[1]);
                 getEnvironment().getCurrentMission().nextGoal();
-                if (getEnvironment().getCurrentMission().isOver())
-                    status = Status.CLOSEPROBLEM;
                 break;
             case REPORT:
                 status = doReport();
-                break;
-            default:
-                status = Status.CLOSEPROBLEM;
+                getEnvironment().getCurrentMission().nextGoal();
                 break;
         }
+        
+        if (getEnvironment().getCurrentMission().isOver())
+                status = Status.CLOSEPROBLEM;
         
         return status;
     }
     
+    public void destFounder() {
+        if (destFounded == false) {
+            ArrayList<String> list;
+            list = this.DFGetAllProvidersOf("TYPE DEST");
+            for(int i = 0 ; i < list.size(); i++){
+                if(this.DFHasService(list.get(i), sessionKey)){
+                    dest = list.get(i);
+                    Alert("FOUND AGENT DEST" + dest);
+                }
+            }
+
+            destFounded = true;
+        }
+    }
+        
+    
     public Status doReport(){
-        return Status.CLOSEPROBLEM;
+        destFounder();
+        report += ";";
+        this.outbox = new ACLMessage();
+        outbox.setSender(getAID());
+        outbox.addReceiver(new AID(dest, AID.ISLOCALNAME));
+        outbox.setContent(report);
+        this.LARVAsend(outbox);
+        Info("Sended report");
+        return Status.SOLVEPROBLEM;
     }
     
     public Status doMoveIn() {        
@@ -263,8 +291,6 @@ public class ITT extends LARVAFirstAgent {
             Message("The problem " + problem + " has been solved");
             goalInitiated = false;
             getEnvironment().getCurrentMission().nextGoal();
-            if (getEnvironment().getCurrentMission().isOver())
-                return Status.CLOSEPROBLEM;
             
             return Status.SOLVEPROBLEM;
         }
@@ -301,6 +327,7 @@ public class ITT extends LARVAFirstAgent {
             Error("Unable to execute action " + action + " due to " + session.getContent());
             return false;
         }
+        
         return true;
     }
 
@@ -363,12 +390,22 @@ public class ITT extends LARVAFirstAgent {
     }
 
     public double goAvoid(Environment E, Choice a) {
-        if (a.getName().equals("RIGHT")) {
-            nextWhichwall = "LEFT";
-            nextdistance = E.getDistance();
-            nextPoint = E.getGPS();
-            return Choice.ANY_VALUE;
+        if (E.isTargetLeft()) {
+            if (a.getName().equals("LEFT")) {
+                nextWhichwall = "RIGHT";
+                nextdistance = E.getDistance();
+                nextPoint = E.getGPS();
+                return Choice.ANY_VALUE;
+            }
+        } else {
+            if (a.getName().equals("RIGHT")) {
+                nextWhichwall = "LEFT";
+                nextdistance = E.getDistance();
+                nextPoint = E.getGPS();
+                return Choice.ANY_VALUE;
+            }
         }
+        
         return Choice.MAX_UTILITY;
     }
 
@@ -383,6 +420,20 @@ public class ITT extends LARVAFirstAgent {
             return goKeepOnWall(E, a);
         } else {
             return goRevolveWallLeft(E, a);
+        }
+    }
+    
+    public double goFollowWallRight(Environment E, Choice a) {
+        if (E.isFreeFrontRight()) {
+            return goTurnOnWallRight(E, a);
+        } else if (E.isTargetFrontLeft()
+                && E.isFreeFrontLeft()
+                && E.getDistance() < point.planeDistanceTo(E.getTarget())) {
+            return goStopWallRight(E, a);
+        } else if (E.isFreeFront()) {
+            return goKeepOnWall(E, a);
+        } else {
+            return goRevolveWallRight(E, a);
         }
     }
 
@@ -400,9 +451,24 @@ public class ITT extends LARVAFirstAgent {
         return Choice.MAX_UTILITY;
 
     }
+    
+    public double goTurnOnWallRight(Environment E, Choice a) {
+        if (a.getName().equals("RIGHT")) {
+            return Choice.ANY_VALUE;
+        }
+        return Choice.MAX_UTILITY;
+
+    }
 
     public double goRevolveWallLeft(Environment E, Choice a) {
         if (a.getName().equals("RIGHT")) {
+            return Choice.ANY_VALUE;
+        }
+        return Choice.MAX_UTILITY;
+    }
+    
+    public double goRevolveWallRight(Environment E, Choice a) {
+        if (a.getName().equals("LEFT")) {
             return Choice.ANY_VALUE;
         }
         return Choice.MAX_UTILITY;
@@ -415,16 +481,27 @@ public class ITT extends LARVAFirstAgent {
         }
         return Choice.MAX_UTILITY;
     }
+    
+    public double goStopWallRight(Environment E, Choice a) {
+        if (a.getName().equals("LEFT")) {
+            this.resetAutoNAV();
+            return Choice.ANY_VALUE;
+        }
+        return Choice.MAX_UTILITY;
+    }
 
     @Override
     protected double U(Environment E, Choice a) {
         if (whichWall.equals("LEFT")) {
             return goFollowWallLeft(E, a);
+        } else if (whichWall.equals("RIGHT")) {
+            return goFollowWallRight(E, a);
         } else if (!E.isFreeFront()) {
             return goAvoid(E, a);
         } else {
             return goAhead(E, a);
         }
+        
     }
 
     protected boolean MyReadPerceptions() {
@@ -474,59 +551,38 @@ public class ITT extends LARVAFirstAgent {
     public String easyPrintPerceptions() {
         String res;
         int matrix[][];
-        if (!logger.isEcho()) {
-            return "";
-        }
+
         if (getEnvironment() == null) {
             Error("Environment is unacessible, please setupEnvironment() first");
             return "";
         }
-        if (!showPerceptions) {
-            return "";
-        }
         res = "\n\nReading of sensors\n";
-        if (getEnvironment().getName() == null) {
+        if (E.getName() == null) {
             res += emojis.WARNING + " UNKNOWN AGENT";
             return res;
         } else {
-            res += emojis.ROBOT + " " + getEnvironment().getName();
+            res += emojis.ROBOT + " " + E.getName();
         }
         res += "\n";
-        res += String.format("%10s: %05d W %05d W %05d W\n", "ENERGY",
-                getEnvironment().getEnergy(), getEnvironment().getEnergyburnt(), myEnergy);
-        res += String.format("%10s: %15s\n", "POSITION", getEnvironment().getGPS().toString());
-        res += "PAYLOAD " + getEnvironment().getPayload() + " m" + "\n";
-        res += String.format("%10s: %05d m\n", "X", getEnvironment().getGPS().getXInt())
-                + String.format("%10s: %05d m\n", "Y", getEnvironment().getGPS().getYInt())
-                + String.format("%10s: %05d m\n", "Z", getEnvironment().getGPS().getZInt())
-                + String.format("%10s: %05d m\n", "MAXLEVEL", getEnvironment().getMaxlevel())
-                + String.format("%10s: %05d m\n", "MAXSLOPE", getEnvironment().getMaxslope());
-        res += String.format("%10s: %05d m\n", "GROUND", getEnvironment().getGround());
-        res += String.format("%10s: %05d º\n", "COMPASS", getEnvironment().getCompass());
-        if (getEnvironment().getTarget() == null) {
+        res += String.format("%10s: %05d W\n", "ENERGY", E.getEnergy());
+        res += String.format("%10s: %15s\n", "POSITION", E.getGPS().toString());
+//        res += "PAYLOAD "+E.getPayload()+" m"+"\n";
+        res += String.format("%10s: %05d m\n", "X", E.getGPS().getXInt())
+                + String.format("%10s: %05d m\n", "Y", E.getGPS().getYInt())
+                + String.format("%10s: %05d m\n", "Z", E.getGPS().getZInt())
+                + String.format("%10s: %05d m\n", "MAXLEVEL", E.getMaxlevel())
+                + String.format("%10s: %05d m\n", "MAXSLOPE", E.getMaxslope());
+        res += String.format("%10s: %05d m\n", "GROUND", E.getGround());
+        res += String.format("%10s: %05d º\n", "COMPASS", E.getCompass());
+        if (E.getTarget() == null) {
             res += String.format("%10s: " + "!", "TARGET");
         } else {
-            res += String.format("%10s: %05.2f m\n", "DISTANCE", getEnvironment().getDistance());
-            res += String.format("%10s: %05.2f º\n", "ABS ALPHA", getEnvironment().getAngular());
-            res += String.format("%10s: %05.2f º\n", "REL ALPHA", getEnvironment().getRelativeAngular());
-        }
-        res += "\nVISUAL ABSOLUTE\n";
-        matrix = getEnvironment().getAbsoluteVisual();
-        for (int y = 0; y < matrix[0].length; y++) {
-            for (int x = 0; x < matrix.length; x++) {
-                res += printValue(matrix[x][y]);
-            }
-            res += "\n";
-        }
-        for (int x = 0; x < matrix.length; x++) {
-            if (x != matrix.length / 2) {
-                res += "----";
-            } else {
-                res += "[  ]-";
-            }
+            res += String.format("%10s: %05.2f m\n", "DISTANCE", E.getDistance());
+            res += String.format("%10s: %05.2f º\n", "ABS ALPHA", E.getAngular());
+            res += String.format("%10s: %05.2f º\n", "REL ALPHA", E.getRelativeAngular());
         }
         res += "\nVISUAL RELATIVE\n";
-        matrix = getEnvironment().getRelativeVisual();
+        matrix = E.getRelativeVisual();
         for (int y = 0; y < matrix[0].length; y++) {
             for (int x = 0; x < matrix.length; x++) {
                 res += printValue(matrix[x][y]);
@@ -540,17 +596,8 @@ public class ITT extends LARVAFirstAgent {
                 res += "[  ]-";
             }
         }
-        res += "\nVISUAL POLAR\n";
-        matrix = getEnvironment().getPolarVisual();
-        for (int y = 0; y < matrix[0].length; y++) {
-            for (int x = 0; x < matrix.length; x++) {
-                res += printValue(matrix[x][y]);
-            }
-            res += "\n";
-        }
-        res += "\n";
         res += "LIDAR RELATIVE\n";
-        matrix = getEnvironment().getRelativeLidar();
+        matrix = E.getRelativeLidar();
         for (int y = 0; y < matrix[0].length; y++) {
             for (int x = 0; x < matrix.length; x++) {
                 res += printValue(matrix[x][y]);
@@ -565,9 +612,8 @@ public class ITT extends LARVAFirstAgent {
             }
         }
         res += "\n";
-        return res
-                + "\n" + this.Prioritize(E, A).toString()
-                + "\nWall:\n" + whichWall + "\n";
+        res += "Decision Set: " + A.toString() + "\n";
+        return res;
     }
 
     public String easyPrintPerceptions(Environment E, DecisionSet A) {
@@ -670,11 +716,28 @@ public class ITT extends LARVAFirstAgent {
     protected Status doQueryPeople(String type) {
         Info("Querying people " + type);
         outbox = session.createReply();
-        outbox.setContent("Query " + type.toUpperCase() + " session " + sessionKey);
+        outbox.setContent("Query " + type.toUpperCase() 
+                          + " session " + sessionKey);
         this.LARVAsend(outbox);
         session = LARVAblockingReceive();
         getEnvironment().setExternalPerceptions(session.getContent());
-        Message("Found " + getEnvironment().getPeople().length + " " + type + " in " + getEnvironment().getCurrentCity());
+        Message("Found " + getEnvironment().getPeople().length + " " 
+                + type + " in " +getEnvironment().getCurrentCity());
+        
+        if(report == ""){
+            report += "REPORT;" + getEnvironment().getCurrentCity() + " " 
+                      + type.toLowerCase() + " " 
+                      + getEnvironment().getPeople().length;
+            actualCity = getEnvironment().getCurrentCity();
+        }else{
+            if (!getEnvironment().getCurrentCity().equals(actualCity)){
+                report += ";" + getEnvironment().getCurrentCity();
+                actualCity = getEnvironment().getCurrentCity();
+            }
+
+            report += " " + type.toLowerCase() + " " 
+                      + getEnvironment().getPeople().length;                
+        }
         
         return myStatus;
     }
